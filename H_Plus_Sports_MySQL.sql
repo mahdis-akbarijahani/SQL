39d9fcee3fb6c85c823262f6b8ba1091176c4b37
mahdisakbari <60046641+mahdisakbari@users.noreply.github.com>
2024-08-01 20:32:49 +0200

    Update H_Plus_Sports_MySQL.sql

--- H_Plus_Sports_MySQL.sql
+++ H_Plus_Sports_MySQL.sql
@@ -1,23 +1,3 @@
-﻿# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: localhost (MySQL 5.7.23)
-# Database: hplus
-# Generation Time: 2018-09-14 21:55:14 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 # Dump of table Customer
 # ------------------------------------------------------------
@@ -1919,16 +1899,6 @@ VALUES
 	(149,'Walter','Rivera','wrivera1d@hplussport.com','(210)179-0406','6 Manitowish Terrace','San Antonio','Texas','78265'),
 	(150,'Teresa','Riley','triley1e@hplussport.com','(651)210-8670','9491 Jana Lane','Saint Paul','Minnesota','55146');
 
-/*!40000 ALTER TABLE `Salesperson` ENABLE KEYS */;
-UNLOCK TABLES;
 
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 GRANT ALL PRIVILEGES ON hplus.* TO 'root'@'localhost';
